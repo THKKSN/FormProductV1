dc8067b7c68949a5d3f5bea49c6067819c0e609f
Work <thakronkhamsingnok@gmail.com>
2024-07-26 16:06:54 +0700

    add circlegraph to ResultPage

---
+++ src/compornents/CircularGraph.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Doughnut } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import styles from "../style/CircularGraph.module.css";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const CircularGraph = ({ data, labels, evaluation }) => {
+  const chartData = {
+    labels: labels,
+    datasets: [
+      {
+        data: data,
+        backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(0, 250, 50, 0.2)"],
+        borderColor: ["rgba(255, 99, 132, 1)", "rgba(0, 250, 50, 1)"],
+        borderWidth: 1,
+        hoverOffset: 30, // This will add an offset to the segment when hovered
+
+      },
+    ],
+  };
+
+  const options = {
+    plugins: {
+      tooltip: {
+        callbacks: {
+          label: function (context) {
+            let label = context.label || "%";
+            if (label) {
+              label += ": ";
+            }
+            if (context.parsed !== null) {
+              label += context.parsed + "%";
+            }
+            return label;
+          },
+        },
+      },
+    },
+    elements: {
+      center: {
+        text: `${evaluation}%`,
+        color: "#000000",
+        fontStyle: "Sarabun",
+        sidePadding: 20,
+      },
+    },
+  };
+
+  return (
+    <div className={styles.graphContainer}>
+      <Doughnut data={chartData} options={options} />
+      <div className={styles.chartText}>
+        <h1>{evaluation}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default CircularGraph;
--- src/compornents/ResultPage.js
+++ src/compornents/ResultPage.js
@@ -1,38 +1,141 @@
 import React from "react";
-import style from "../style/Result.module.css";
+import CircularGraph from "./CircularGraph"; // ตรวจสอบเส้นทางให้ถูกต้อง
+import styles from "../style/Result.module.css";
 
-const ResultPage = ({ initialFormValues }) => {
+const ResultPage = () => {
+  const labels = [
+    "ผลสัมฤทธิ์ของงาน (ร้อยละ) (C)",
+    "สมรรถนะกำลังพล (ร้อยละ) (D)",
+  ];
+  const data = [70, 30]; // ข้อมูลตัวอย่างสำหรับ C และ D
+  const evaluation = (data[0] + data[1]) / 2; // คำนวณเปอร์เซ็นต์รวมของการประเมิน
+
   return (
     <div>
-      <h3>ตอนที่ 4 ผลการประเมิน</h3>
-      <div className={style.tableContainer}>
-        <h5>ผลการประเมิน (ร้อยละ) (C)+(D)</h5>
-        <h1>400</h1>
-      </div>
-      <div className={style.tableContainer}>
-        <h5>ผลสัมฤทธิ์ของงาน(ร้อยละ) (C) (A÷5)</h5>
-        <h5>สมรรถนะกำลังพล (ร้อยละ) (D) (B÷5)</h5>
-      </div>
-      <div className={style.tableContainer}>
-        <h6>400</h6>
-        <h6>400</h6>
-      </div>
-
-      <br></br>
-      <div className={style.tableContainer}>
-        <h5>แบบประเมินค่า</h5>
-        <h5>ค่าคะแนน</h5>
-      </div>
-      <div className={style.centerContainer}>
-        <p className={style.center}>ผลสัมฤทธิ์ของงาน</p>
-      </div>
-      <div className={style.centerContainer}>
-        <p className={style.center}>1.1 กลุ่มงานประจำ (เต็ม 100 คะแนน)</p>
-      </div>
-      <div className={style.sectionContent}>
-        <div className={style.sectionItem}>
-          <p>1.1.1 ปริมาณงาน</p>
-          <p>500</p>
+      <h2>ตอนที่ 4 ผลการประเมิน</h2>
+      <div className={styles.container}>
+        <div className={styles.graphContainer}>
+          <CircularGraph data={data} labels={labels} evaluation={evaluation} />
+        </div>
+        <div className={styles.tableContainer}>
+          <table>
+            <thead>
+              <tr>
+                <th>แบบประเมินค่า</th>
+                <th>คะแนน</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td colSpan="2">ผลสัมฤทธิ์ของงาน</td>
+              </tr>
+              <tr>
+                <td colSpan="2">1.1 กลุ่มงานประจำ (เต็ม 100 คะแนน)</td>
+              </tr>
+              <tr>
+                <td>1.1.1 ปริมาณงาน</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>1.1.2 ผลสำเร็จของงาน</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>1.1.3 ความเร่งด่วนในการทำงาน</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>1.1.4 ความละเอียด</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>1.1.5 การประพฤติงาน</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td colSpan="2">1.2 กลุ่มงานนอกแบบ (เต็ม 250 คะแนน)</td>
+              </tr>
+              <tr>
+                <td>1.2.1 ผลสำเร็จของงาน</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>1.2.2 คุณภาพของงาน</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>1.2.3 ความเร่งด่วนในการทำงาน</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>1.2.4 ความละเอียด</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>รวมคะแนนผลสัมฤทธิ์ของงาน (A) (เต็ม 350 คะแนน)</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td colSpan="2">สมรรถนะที่คาดหวัง</td>
+              </tr>
+              <tr>
+                <td>2.1.1 มุ่งศึกษา</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>2.1.2 การวิจัย</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>2.2.1 ความสามารถในการคิดเชิงสังเคราะห์</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>2.2.2 มุ่งผลงานผู้รับ</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>2.2.3 ความสามารถ</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>2.3.1 การปฏิสัมพันธ์</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>2.3.2 การทำงานเป็นทีม</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>2.4.1 ความซื่อสัตย์และมีสิทธิ์</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>2.4.2 การมองโลก</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>2.5.1 การมองโลกในเชิงบวก</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>2.5.2 การแก้ปัญหาที่เกิดขึ้น</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>2.6.1 การประสานความสัมพันธ์ระหว่างผู้รับคำแนะนำ</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>2.6.2 หลักเกณฑ์และมาตรการที่เกิดขึ้น</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>รวมคะแนนสมรรถนะที่คาดหวัง (B) (เต็ม 150 คะแนน)</td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
